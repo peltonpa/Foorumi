a6912542e82140e471f377a8e92089de2d864179
Paavo Peltonen <peltonpa@dhcp-asv-98.helsinki.fi>
2016-08-04 19:47:24 +0300

    Suunnitelmat lisätty: etusivu.html, keskustelualue.html, ketju.html ja kirjautuminen.html

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,28 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+      View::make('suunnitelmat/etusivu.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
+    
+    public static function etusivu(){
+      View::make('suunnitelmat/etusivu.html');
+    }
+    
+    public static function keskustelualue() {
+        View::make('suunnitelmat/keskustelualue.html');
+    }
+    
+    public static function ketju() {
+        View::make('suunnitelmat/ketju.html');
+    }
+    
+    public static function kirjautuminen() {
+        View::make('suunnitelmat/kirjautuminen.html');
+    }
+    
   }
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,26 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/etusivu', function() {
+    HelloWorldController::etusivu();
+});
+
+$routes->get('/keskustelualue', function() {
+    HelloWorldController::keskustelualue();
+});
+
+$routes->get('/ketju', function() {
+    HelloWorldController::ketju();
+});
+
+$routes->get('/kirjautuminen', function() {
+HelloWorldController::kirjautuminen();
+});
+
